a643dd3d90d2783b8ce7a18f1334cb847896b18b
ZztLeo <a540057734@hotmail.com>
2019-10-16 21:04:15 +0800

    delete old version code

--- OFC_GGNN/dataloader.py
+++
@@ -1,6 +0,0 @@
-from torch.utils.data import DataLoader
-
-class alarmDataloader(DataLoader):
-
-    def __init__(self, *args, **kwargs):
-        super(alarmDataloader, self).__init__(*args, **kwargs)
--- OFC_GGNN/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Pah to alarm data
-AlarmdataPath="alarm_data/create_trainset.txt"
-
-#Path to graph structure
-GraphstructurePath="alarm_data/alarm_graph_test_reason1.txt"
-
-python main.py --alarm_data $AlarmdataPath --Graph_structure $GraphstructurePath 
